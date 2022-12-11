e742706d4dde7803a4db30c832d41ef57ff7a82e
cleitondcarmo <cleitondcarmo@hotmail.com>
2022-12-11 00:31:16 -0300

    Updates

--- assets/ContactView.6e91d9eb.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as t,c as e}from"./index.5383d9dc.js";const o={},n={class:"contact"};function s(a,r){return t(),e("div",n)}const f=c(o,[["render",s]]);export{f as default};
---
+++ assets/ContactView.94c0d41b.js
@@ -0,0 +1,1 @@
+import{_ as c,o as t,c as e}from"./index.215661e2.js";const o={},n={class:"contact"};function s(a,r){return t(),e("div",n)}const f=c(o,[["render",s]]);export{f as default};
